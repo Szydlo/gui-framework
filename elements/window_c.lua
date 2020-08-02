59884daaf72c591bf3757c5efd01bd57e3da1e4b
unknown <szydlogaming@gmail.com>
2020-08-02 17:24:27 +0200

    Fixed moving in window

--- elements/window_c.lua
+++ elements/window_c.lua
@@ -9,67 +9,29 @@ function Window:constructor(pos, size, color)
 	self.moveWindow = false
 	self.lastPos = getRealCursorPosition()
 	self.cursorPos = Vector2(0,0)
+
+	self.cursormov = bind(Window.cursorMove, self)
+	addEventHandler("onClientCursorMove", root, self.cursormov)
 end
 
 function Window:click()
 	if self.movable then
 		self.lastPos = getRealCursorPosition()
+		self.lastpos = self.pos
+	end
+end
+
+function Window:cursorMove(_, _, x, y)
+	if isMouseInPosition(self.pos, self.size) and getKeyState("mouse1") then
+		local delta = Vector2(x - self.lastPos.x, y - self.lastPos.y)
+		self.pos = self.lastpos + delta
 	end
 end
 
 function Window:draw()
-	if getKeyState("mouse1") and isMouseInPosition(self.pos - self.cursorPos, self.size) then
-		local realpos = getRealCursorPosition()
-		self.cursorPos = self.lastPos - realpos
-	end
-
-	dxDrawRectangle(self.pos - self.cursorPos, self.size, self.color)
-end
-
---wn = new(Window, Vector2(0, 0), Vector2(300, 300), tocolor(32,32,32))
-
-showCursor(true)
-
---[[
-Window = inherit(Element)
---KURWA NIE DZIALA PRZESUWANIE
-function Window:constructor(pos, size, color)
-	self:init(pos, size, color)
-	self.movable =	true
-
-	self.moveWindow = false
-	self.lastPos = getRealCursorPosition()
-	self.cursorPos = Vector2(0,0)
-end
-
-function Window:click()
-	if self.movable then
-		self.lastPos = getRealCursorPosition()
-		self.pressed = true
-	end
-end
-
-function Window:outClick()
-	if self.movable then
-		self.pressed = false
-	end
-end
-
-function Window:draw()
-	outputChatBox(self.pos.x)
-
-if getKeyState("mouse1") and isMouseInPosition(self.pos - self.cursorPos, self.size) then
-
-	if self.pressed then
-		local realpos = getRealCursorPosition()
-		self.cursorPos = self.lastPos - realpos
-	end
-
-	dxDrawRectangle(self.pos - self.cursorPos, self.size, self.color)
+	dxDrawRectangle(self.pos, self.size, self.color)
 end
 
 wn = new(Window, Vector2(0, 0), Vector2(300, 300), tocolor(32,32,32))
 
 showCursor(true)
-
-]]
